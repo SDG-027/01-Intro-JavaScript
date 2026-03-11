d33e38ffe0bbafcbe7f298a1bc44aeed95ff0513
Stephan Ullmann <stephan.ullmann@posteo.de>
2026-03-11 15:16:15 +0100

    JS Objects - Übungen

---
+++ 07-JS-Objekte-I-Korrekturen/002_Destructuring/script.js
@@ -0,0 +1,33 @@
+// Object Literals
+// ==============
+
+// **Objective:**
+
+// Learn how to use destructuring to extract values from objects and arrays in JavaScript.
+
+// **Instructions:**
+const fruits = ["apple", "banana", "cherry", "date"];
+
+// Simple Array Destructuring Extract the first two elements from the fruits array and store them in variables fruit1 and fruit2.
+
+// Skipping Elements in Array Destructuring Extract the first and third elements from the fruits array, skipping the second element.
+
+// Simple Object Destructuring Extract the name and age properties from the person object.
+const person = {
+  name: "John Doe",
+  age: 30,
+  address: {
+    city: "New York",
+    zip: "10001",
+  },
+};
+
+// Nested Object Destructuring Extract the city from the address property of the person object.
+
+// Destructuring in Function Parameters displayPerson is function that takes a person object and logs the name and age properties using dot notation. Modify the function to destructure the name and age properties directly in the parameters. Modify the function to use destructuring in the parameters
+
+function displayPerson(person) {
+  console.log(`Name: ${person.name}, Age: ${person.age}`);
+}
+
+displayPerson(person);
